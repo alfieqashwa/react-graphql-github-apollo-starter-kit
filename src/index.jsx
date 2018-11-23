c4dd0b74d24e900fa4e6a6e5ad0a72deeae4d5b1
alfieqashwa <alfieqahwa@gmail.com>
2018-11-23 15:37:58 +0700

    Modified .giginore

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import App from './App';
+import './style.css';
 import * as serviceWorker from './serviceWorker';
+
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
