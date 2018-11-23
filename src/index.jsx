6fc36845f34a731be5365401f9cc70bc1d73625d
alfieqashwa <alfieqahwa@gmail.com>
2018-11-23 21:39:00 +0700

    Added conditional Error func

--- src/index.jsx
+++ src/index.jsx
@@ -25,11 +25,15 @@ const httpLink = new HttpLink({
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors) {
-    // ...
+    graphQLErrors.map(({ message, locations, path }) => {
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      );
+    });
   }
 
   if (networkError) {
-    // ...
+    console.log(`[Network error]: ${networkError}`);
   }
 });
 
