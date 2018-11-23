a55538dcc6ab5337c6d941db60194330d5e5e40e
alfieqashwa <alfieqahwa@gmail.com>
2018-11-23 21:22:49 +0700

    still facing problem: Error: Network error: Response not successful: Received status code 401

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import './style.css';
+
+import Profile from '../Profile';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Cello Apollo & GraphQL</h1>
-      </div>
-    );
+    return <Profile />;
   }
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import * as serviceWorker from './serviceWorker';
@@ -20,14 +23,31 @@ const httpLink = new HttpLink({
   }
 });
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    // ...
+  }
+
+  if (networkError) {
+    // ...
+  }
+});
+
+const link = ApolloLink.from([errorLink, httpLink]);
+
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
