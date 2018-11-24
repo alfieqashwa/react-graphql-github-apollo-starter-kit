2e8c19c1e0aa724b9206ce70e92963ebd1f70a67
alfieqashwa <alfieqahwa@gmail.com>
2018-11-25 02:55:38 +0700

    Finished GraphQL Caching of Queries w/ Apollo Client in React

--- src/App/Navigation/index.jsx
+++ src/App/Navigation/index.jsx
@@ -7,40 +7,28 @@ import Button from '../../Button';
 import Input from '../../Input';
 
 import './style.css';
-class Navigation extends Component {
-  state = {
-    organizationName: 'the-road-to-learn-react'
-  };
 
-  onOrganizationSearch = value => {
-    this.setState({ organizationName: value });
-  };
+const Navigation = ({
+  location: { pathname },
+  organizationName,
+  onOrganizationSearch
+}) => (
+  <header className="Navigation">
+    <div className="Navigation-Link">
+      <Link to={routes.PROFILE}>Profile</Link>
+    </div>
+    <div className="Navigation-link">
+      <Link to={routes.ORGANIZATION}>Organization</Link>
+    </div>
 
-  render() {
-    const {
-      location: { pathname }
-    } = this.props;
-
-    return (
-      <header className="Navigation">
-        <div className="Navigation-Link">
-          <Link to={routes.PROFILE}>Profile</Link>
-        </div>
-        <div className="Navigation-link">
-          <Link to={routes.ORGANIZATION}>Organization</Link>
-        </div>
-
-        {pathname === routes.ORGANIZATION && (
-          <OrganizationSearch
-            organizationName={this.state.organizationName}
-            onOrganizationSearch={this.onOrganizationSearch}
-          />
-        )}
-      </header>
-    );
-  }
-}
-
+    {pathname === routes.ORGANIZATION && (
+      <OrganizationSearch
+        organizationName={organizationName}
+        onOrganizationSearch={onOrganizationSearch}
+      />
+    )}
+  </header>
+);
 class OrganizationSearch extends Component {
   state = {
     value: this.props.organizationName
