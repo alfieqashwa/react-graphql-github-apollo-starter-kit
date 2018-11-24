17f5ee04820de00f235d1bb686ba436e5e1d0575
alfieqashwa <alfieqahwa@gmail.com>
2018-11-25 01:23:35 +0700

    Finished GraphQL Pagination w/ Apollo Client in React

--- src/Profile/index.jsx
+++ src/Profile/index.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 
 import RepositoryList, { REPOSITORY_FRAGMENT } from '../Repository';
 import Loading from '../Loading';
 import ErrorMessage from '../Error';
 
 const GET_REPOSITORIES_OF_CURRENT_USER = gql`
-  {
+  query($cursor: String) {
     viewer {
-      repositories(first: 5, orderBy: { direction: DESC, field: STARGAZERS }) {
+      repositories(
+        first: 10
+        orderBy: { direction: DESC, field: STARGAZERS }
+        after: $cursor
+      ) {
         edges {
           node {
             ...repository
           }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
         }
       }
     }
@@ -21,18 +29,31 @@ const GET_REPOSITORIES_OF_CURRENT_USER = gql`
   ${REPOSITORY_FRAGMENT}
 `;
 
-const Profile = ({ data, loading, error }) => {
-  if (error) {
-    return <ErrorMessage error={error} />;
-  }
+const Profile = () => (
+  <Query
+    query={GET_REPOSITORIES_OF_CURRENT_USER}
+    notifyOnNetworkStatusChange={true}
+  >
+    {({ data, loading, error, fetchMore }) => {
+      if (error) {
+        return <ErrorMessage error={error} />;
+      }
 
-  const { viewer } = data;
+      const { viewer } = data;
 
-  if (loading || !viewer) {
-    return <Loading />;
-  }
+      if (loading && !viewer) {
+        return <Loading />;
+      }
 
-  return <RepositoryList repositories={viewer.repositories} />;
-};
+      return (
+        <RepositoryList
+          loading={loading}
+          repositories={viewer.repositories}
+          fetchMore={fetchMore}
+        />
+      );
+    }}
+  </Query>
+);
 
-export default graphql(GET_REPOSITORIES_OF_CURRENT_USER)(Profile);
+export default Profile;
